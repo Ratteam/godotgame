b186607385dc84448e81317daee433adeeb7c6a5
xuyanan <1548582000@qq.com>
2020-08-21 07:02:00 +0800

    P10制作草消失动画

--- ARPG/Scripts/Player.gd
+++ ARPG/Scripts/Player.gd
@@ -7,6 +7,15 @@ const MAX_SPEED = 80
 # 摩擦力
 const FRICTION = 500
 
+# 自定义玩家状态
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+# 玩家状态
+var state = MOVE
 # 坐标点
 var velocity = Vector2.ZERO
 
@@ -15,8 +24,22 @@ onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
+func _ready():
+	animationTree.active = true
+
 # 每帧调用
-func _physics_process(delta):
+func _process(delta):
+	# 判断玩家状态
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+
+# 移动函数
+func move_state(delta):
 	# 重置坐标
 	var input_vector = Vector2.ZERO
 	# 获取坐标点
@@ -29,6 +52,7 @@ func _physics_process(delta):
 		# 播放移动动画
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Run")
 		# 开始移动
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
@@ -38,3 +62,16 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	# 依据坐标移动
 	velocity = move_and_slide(velocity)
+	# 监听键盘事件
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+		
+# 攻击函数
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+	
+# 监听攻击动画结束函数
+func attack_animation_finished():
+	state = MOVE
+	
