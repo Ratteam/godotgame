844d5e3911993cdee9abdee54e469ec11a6a8791
xuyanan <1548582000@qq.com>
2020-08-28 06:30:28 +0800

    玩家滚动完成

--- ARPG/Scripts/Player.gd
+++ ARPG/Scripts/Player.gd
@@ -4,6 +4,8 @@ extends KinematicBody2D
 const ACCELERATION = 500
 # 最大速度
 const MAX_SPEED = 80
+# 滚动速度
+const ROLL_SPEED = 120
 # 摩擦力
 const FRICTION = 500
 
@@ -18,6 +20,7 @@ enum {
 var state = MOVE
 # 坐标点
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.ZERO
 
 # 获取子节点
 onready var animationPlayer = $AnimationPlayer
@@ -34,7 +37,7 @@ func _process(delta):
 		MOVE:
 			move_state(delta)
 		ROLL:
-			pass
+			roll_state(delta)
 		ATTACK:
 			attack_state(delta)
 
@@ -49,10 +52,12 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 	# 判断是否移动
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
 		# 播放移动动画
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 		animationState.travel("Run")
 		# 开始移动
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
@@ -60,16 +65,33 @@ func move_state(delta):
 		animationState.travel("Idle")
 		# 停止移动时，添加摩擦力
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	# 依据坐标移动
-	velocity = move_and_slide(velocity)
+	move()
 	# 监听键盘事件
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
+		
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
-		
+
+# 滚动函数
+func roll_state(delta):
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel("Roll")	
+	move()
+
+# 依据坐标移动
+func move():
+	velocity = move_and_slide(velocity)
+	
 # 攻击函数
 func attack_state(delta):
 	velocity = Vector2.ZERO
 	animationState.travel("Attack")
+	
+# 翻滚结束
+func roll_animation_finished():
+	velocity = velocity * 0.8
+	state = MOVE
 	
 # 监听攻击动画结束函数
 func attack_animation_finished():
