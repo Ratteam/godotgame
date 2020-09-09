eb185b66a72cf0d6263f0c344c6bb007d9fa8091
xuyanan <1548582000@qq.com>
2020-09-09 12:02:05 +0800

    敌人巡山功能

--- ARPG/Scripts/Bat.gd
+++ ARPG/Scripts/Bat.gd
@@ -18,6 +18,7 @@ onready var playerDetectionZoom = $PlayerDetectionZoom
 onready var sprite = $AnimatedSprite
 onready var hurtbox = $Hurtbox
 onready var softCollision = $SoftCollision
+onready var wanderController = $WanderController
 
 var knockback = Vector2.ZERO
 var velocity = Vector2.ZERO
@@ -31,12 +32,21 @@ func _physics_process(delta):
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 			seek_player()
+			if wanderController.get_time_left() == 0:
+				state = pick_random_state([IDLE,WANDER])
+				wanderController.start_wander_timer(rand_range(1,3))
 		WANDER:
-			pass
+			seek_player()
+			if wanderController.get_time_left() == 0:
+				state = pick_random_state([IDLE,WANDER])
+				wanderController.start_wander_timer(rand_range(1,3))
+			var direction = global_position.direction_to(wanderController.target_position)
+			velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta) 
+				
 		CHASE:
 			var player = playerDetectionZoom.player
 			if player != null:
-				var direction = (player.global_position - global_position).normalized()
+				var direction = global_position.direction_to(player.global_position)
 				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta) 
 			else:
 				state = IDLE
@@ -49,6 +59,10 @@ func _physics_process(delta):
 func seek_player():
 	if playerDetectionZoom.can_see_player():
 		state = CHASE
+
+func pick_random_state(state_list):
+	state_list.shuffle()
+	return state_list.pop_front()
 
 func _on_Stats_no_health():
 	queue_free()
